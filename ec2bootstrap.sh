5c414f673730c0f5a7b675687ed1f8202dff85d9
Raúl Reguillo Carmona <raul.reguillo@bbva.com>
2017-12-20 12:31:52 +0100

    [IMP] update with no cache dir while installing pip dependencies

--- ec2bootstrap.sh
+++ ec2bootstrap.sh
@@ -5,6 +5,6 @@ git clone https://github.com/RaulRC/ml-aws.git
 mkdir data
 aws s3 cp s3://poc-ml-op/data/iris.data data/iris.data --region eu-west-1
 aws s3 cp s3://poc-ml-op/resources/requirements.txt requirements.txt --region eu-west-1
-pip install -r requirements.txt
+pip install -r requirements.txt --no-cache-dir
 python ml-aws/hello-world.py
 
