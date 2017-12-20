7977c6c8eedc33c055569e4f8907a8d6bd04d722
Raúl Reguillo Carmona <raul.reguillo@bbva.com>
2017-12-20 11:00:39 +0100

    [IMP] update s3bootstrap with execution

--- s3bootstrap.sh
+++ s3bootstrap.sh
@@ -6,4 +6,5 @@ pip install -r requirements.txt
 mkdir data
 aws s3 cp s3://poc-ml-op/data/iris.data data/iris.data --region eu-west-1
 git clone https://github.com/RaulRC/ml-aws.git
+python hello-world.py
 
