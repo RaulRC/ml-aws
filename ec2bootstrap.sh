fb56fbbcc9157292b6dd6c1a5e8802551d9818f4
Raúl Reguillo Carmona <raul.reguillo@bbva.com>
2017-12-20 15:30:41 +0100

    [FIX] typo

--- ec2bootstrap.sh
+++ ec2bootstrap.sh
@@ -7,4 +7,4 @@ aws s3 cp s3://poc-ml-op/data/iris.data data/iris.data --region eu-west-1
 aws s3 cp s3://poc-ml-op/resources/requirements.txt requirements.txt --region eu-west-1
 pip install -r requirements.txt --no-cache-dir
 python ml-aws/hello-world.py
-
+aws s3 cp output.txt s3://poc-ml-op/resources/output.txt --region eu-west-1
