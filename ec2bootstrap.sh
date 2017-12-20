7a5e6c8ee50400f4f177e0f3947603734ef7225d
Raúl Reguillo Carmona <raul.reguillo@bbva.com>
2017-12-20 11:36:37 +0100

    [FIX] script

--- ec2bootstrap.sh
+++ ec2bootstrap.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 yum update -y
-yum install git
-aws s3 cp s3://poc-ml-op/resources/requirements.txt requirements.txt --region eu-west-1
-pip install -r requirements.txt
+yum install git -y
+git clone https://github.com/RaulRC/ml-aws.git
 mkdir data
 aws s3 cp s3://poc-ml-op/data/iris.data data/iris.data --region eu-west-1
-git clone https://github.com/RaulRC/ml-aws.git
-python hello-world.py
+aws s3 cp s3://poc-ml-op/resources/requirements.txt requirements.txt --region eu-west-1
+pip install -r requirements.txt
+python ml-aws/hello-world.py
 
