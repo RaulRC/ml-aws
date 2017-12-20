cc0d0854786c576b6dc355fca05217f5f21d691e
Raúl Reguillo Carmona <raul.reguillo@bbva.com>
2017-12-20 15:51:33 +0100

    [IMP] commented and self destroy instance

--- ec2bootstrap.sh
+++ ec2bootstrap.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
+# System update
 yum update -y
+# Install Git
 yum install git -y
+# Clone repo
 git clone https://github.com/RaulRC/ml-aws.git
+# Fetch data
 mkdir data
 aws s3 cp s3://poc-ml-op/data/iris.data data/iris.data --region eu-west-1
 aws s3 cp s3://poc-ml-op/resources/requirements.txt requirements.txt --region eu-west-1
+# Install dependencies
 pip install -r requirements.txt --no-cache-dir
+# Execute program
 python ml-aws/hello-world.py
+# Write output
 aws s3 cp output.txt s3://poc-ml-op/resources/output.txt --region eu-west-1
+# Terminate instance
+aws ec2 terminate-instances --instance-ids $(curl -s http://169.254.169.254/latest/meta-data/instance-id) --region eu-west-1
