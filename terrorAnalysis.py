a28a53557ee20b506fc616e14a2ebddabe46192c
Raúl Reguillo Carmona <raul.reguillo@bbva.com>
2017-12-21 22:11:52 +0100

    [IMP] update with less data

--- terrorAnalysis.py
+++ terrorAnalysis.py
@@ -27,7 +27,7 @@ outputFile = open("output.txt", "w")
 # Load dataset
 url = "data/terrorism.csv"
 dataset = pandas.read_csv(url)
-dataset = dataset[["eventid", "iyear", "imonth", "iday", "country"]][:100000]
+dataset = dataset[["eventid", "iyear", "imonth", "iday", "country"]][:10000]
 
 
 # In[32]:
