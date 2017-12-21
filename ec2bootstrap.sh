335b444b9516a35e9ab56edc77db4b4426834d8f
Raúl Reguillo Carmona <raul.reguillo@gmail.com>
2017-12-21 12:03:22 +0100

    [FIX] new example s3 path

--- ec2bootstrap.sh
+++ ec2bootstrap.sh
@@ -7,7 +7,7 @@ yum install git -y
 git clone https://github.com/RaulRC/ml-aws.git
 # Fetch data
 mkdir data
-aws s3 cp s3://poc-ml-op/data/iris.data data/terrorism.csv --region eu-west-1
+aws s3 cp s3://poc-ml-op/data/terrorism.csv data/terrorism.csv --region eu-west-1
 aws s3 cp s3://poc-ml-op/resources/requirements.txt requirements.txt --region eu-west-1
 # Install dependencies
 pip install -r requirements.txt --no-cache-dir
