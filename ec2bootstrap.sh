19e9ad66a5b625b3274ffc097fec52b3a9436cef
Raúl Reguillo Carmona <raul.reguillo@gmail.com>
2017-12-21 10:46:13 +0100

    [IMP] updated script for new and more realistic example

--- ec2bootstrap.sh
+++ ec2bootstrap.sh
@@ -7,13 +7,13 @@ yum install git -y
 git clone https://github.com/RaulRC/ml-aws.git
 # Fetch data
 mkdir data
-aws s3 cp s3://poc-ml-op/data/iris.data data/iris.data --region eu-west-1
+aws s3 cp s3://poc-ml-op/data/iris.data data/terrorism.csv --region eu-west-1
 aws s3 cp s3://poc-ml-op/resources/requirements.txt requirements.txt --region eu-west-1
 # Install dependencies
 pip install -r requirements.txt --no-cache-dir
 # Execute program
-python ml-aws/hello-world.py
+python ml-aws/terrorAnalysis.py
 # Write output
-aws s3 cp output.txt s3://poc-ml-op/resources/output.txt --region eu-west-1
+aws s3 cp output.txt s3://poc-ml-op/resources/output_terror.txt --region eu-west-1
 # Terminate instance
 aws ec2 terminate-instances --instance-ids $(curl -s http://169.254.169.254/latest/meta-data/instance-id) --region eu-west-1
