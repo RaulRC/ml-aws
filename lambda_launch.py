d1dfda2511e0285f6f93d3679c434ac446677181
Raúl Reguillo Carmona <raul.reguillo@bbva.com>
2017-12-24 13:18:25 +0100

    [IMP] update with instance role and name

--- lambda_launch.py
+++ lambda_launch.py
@@ -1,6 +1,7 @@
 import boto3
 
 def lambda_handler(event, context):
+    print boto3.__version__
     """ Lambda handler taking [message] and creating a httpd instance with an echo. """
     # bash script to run:
     #  - update and install httpd (a webserver)
@@ -14,13 +15,15 @@ def lambda_handler(event, context):
     chkconfig httpd on
     echo """ + "hello" + """ > /var/www/html/index.html
     shutdown -H +5"""
-    AMI = 'ami-1a962263'
-    INSTANCE_TYPE = 't2.micro' # instance type to launch.
+    
+    AMI = event['ami']
+    INSTANCE_TYPE = event["instanceType"]
     EC2 = boto3.client('ec2', region_name=event['region'])
-
+    
     print 'Running script:'
     print init_script
-    if event['launch'] == True :
+    
+    if event['launch'] == "True" :
         instance = EC2.run_instances(
             ImageId=AMI,
             InstanceType=INSTANCE_TYPE,
@@ -28,8 +31,24 @@ def lambda_handler(event, context):
             MaxCount=1,
             InstanceInitiatedShutdownBehavior='terminate', # make shutdown in script terminate ec2
             UserData=init_script, # file to run on instance init.
-            KeyName=event["key"]
+            KeyName=event["key"],
+            SecurityGroupIds=[event['groupId']],
+            IamInstanceProfile={
+                'Arn': event["iamInstanceProfileArn"]
+            },
+            TagSpecifications=[
+                {
+                    'ResourceType': 'instance',
+                    'Tags': [
+                        {
+                            'Key': 'Name',
+                            'Value': event['instanceName']
+                        },
+                    ]
+                },
+            ],
         )
+        
         print "New instance created."
         instance_id = instance['Instances'][0]['InstanceId']
         print instance_id
